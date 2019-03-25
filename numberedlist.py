7cbb75e9ae8f0325cfaa190557624cf78ef934e6
Bre <34258642+13re@users.noreply.github.com>
2019-03-24 23:26:35 -0400

    Update numberedlist.py

--- numberedlist.py
+++ numberedlist.py
@@ -1,13 +1,14 @@
 # write a function that takes a list as an argument 
 # and prints out all the values in the list along with the index
-#
+
 # Example: [apple, banana, organge]
 # #1 is apple
 # #2 is banana
-#
+
 # then try:
 # 1st element in the list is apple
 # 2nd element in the list is banana
+
 
 meals = ["breakfast", "lunch", "dinner", "dessert"]
 listlength = len(meals)
@@ -17,6 +18,7 @@ while i < listlength:
     i = i+1 #counter 
     print("#",i,"meal is", meals[a])
 
+    
 #|PRINT__________________
 #| # 1 meal is breakfast
 #| # 2 meal is lunch
